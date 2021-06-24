aff426c56e00f2182411814bd71635b5d82190e8
tompolompo <tomas.lundberg95@gmail.com>
2021-06-24 10:03:33 +0200

    gante

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -3,6 +3,8 @@ import random
 from pingpong.settings import SCREEN_WIDTH, SCREEN_HEIGHT, FRAMERATE
 import pingpong.objects as objects
 import time
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 from pygame.locals import (
     K_ESCAPE,
     KEYDOWN,
